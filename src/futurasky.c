c6e024540386c43854fb05b98b7a0cba4bd239ab
John Hawthorn <john.hawthorn@gmail.com>
2015-05-24 14:45:35 -0700

    Add futura date+time display

--- src/futurasky.c
+++ src/futurasky.c
@@ -1,20 +1,66 @@
 #include <pebble.h>
 
+#define TIME_FRAME      (GRect(0, 2, 144, 168-6))
+#define DATE_FRAME      (GRect(1, 66, 144, 168-62))
+
 static Window *window;
-static TextLayer *text_layer;
+static TextLayer *time_layer;
+static TextLayer *date_layer;
+
+static char time_text[] = "00:00";
+static char date_text[16];
+
+GFont font_date;
+GFont font_time;
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  clock_copy_time_string(time_text, sizeof(time_text));
+  text_layer_set_text(time_layer, time_text);
+
+  if (units_changed & DAY_UNIT) {
+    // Update the date - Without a leading 0 on the day of the month
+    char day_text[16];
+    strftime(day_text, sizeof(day_text), "%B", tick_time);
+    snprintf(date_text, sizeof(date_text), "%s %i", day_text, tick_time->tm_mday);
+    text_layer_set_text(date_layer, date_text);
+  }
+}
+
+static void initial_draw() {
+  time_t now = time(NULL);
+  tick_handler(localtime(&now), SECOND_UNIT | MINUTE_UNIT | HOUR_UNIT | DAY_UNIT);
+}
 
 static void window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
+  window_set_background_color(window, GColorBlack);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  font_date = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FUTURA_18));
+  font_time = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FUTURA_CONDENSED_53));
+
+  /* Time */
+  time_layer = text_layer_create(TIME_FRAME);
+  text_layer_set_text_color(time_layer, GColorWhite);
+  text_layer_set_background_color(time_layer, GColorClear);
+  text_layer_set_font(time_layer, font_time);
+  text_layer_set_text_alignment(time_layer, GTextAlignmentCenter);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(time_layer));
+
+
+  /* Date */
+  date_layer = text_layer_create(DATE_FRAME);
+  text_layer_set_text_color(date_layer, GColorWhite);
+  text_layer_set_background_color(date_layer, GColorClear);
+  text_layer_set_font(date_layer, font_date);
+  text_layer_set_text_alignment(date_layer, GTextAlignmentCenter);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(date_layer));
+
+  initial_draw();
+
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+  text_layer_destroy(time_layer);
 }
 
 static void init(void) {
@@ -28,6 +74,8 @@ static void init(void) {
 }
 
 static void deinit(void) {
+  tick_timer_service_unsubscribe();
+
   window_destroy(window);
 }
 

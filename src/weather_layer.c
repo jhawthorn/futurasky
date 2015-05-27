3466ed251013686dd814e1854821f769497c6e12
John Hawthorn <john.hawthorn@gmail.com>
2015-05-26 21:00:42 -0700

    Just use static globals instead of weather data

--- src/weather_layer.c
+++ src/weather_layer.c
@@ -3,65 +3,59 @@
 #include "weather.h"
 #include "weather_layer.h"
 
-typedef struct {
-  TextLayer *background_layer;
-  TextLayer *temp_layer;
-  GBitmap *icon;
-  BitmapLayer *icon_layer;
-  char temp_str[6];
-} WeatherLayerData;
+static TextLayer *background_layer;
+static TextLayer *temp_layer;
+static GBitmap *bitmap;
+static BitmapLayer *icon_layer;
 
 static GFont large_font, small_font;
 WeatherLayer *weather_layer;
 
 WeatherLayer *weather_layer_create(GRect frame){
   // Create a new layer with some extra space to save our custom Layer infos
-  weather_layer = layer_create_with_data(frame, sizeof(WeatherLayerData));
-  WeatherLayerData *wld = layer_get_data(weather_layer);
+  weather_layer = layer_create(frame);
 
   large_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FUTURA_40));
   small_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FUTURA_35));
 
   // Add background layer
-  wld->background_layer = text_layer_create(GRect(0, 10, 144, 68));
-  text_layer_set_background_color(wld->background_layer, GColorWhite);
-  layer_add_child(weather_layer, text_layer_get_layer(wld->background_layer));
+  background_layer = text_layer_create(GRect(0, 10, 144, 68));
+  text_layer_set_background_color(background_layer, GColorWhite);
+  layer_add_child(weather_layer, text_layer_get_layer(background_layer));
 
   // Add temperature layer
-  wld->temp_layer = text_layer_create(GRect(70, 19, 72, 80));
-  text_layer_set_background_color(wld->temp_layer, GColorClear);
-  text_layer_set_text_alignment(wld->temp_layer, GTextAlignmentCenter);
-  text_layer_set_font(wld->temp_layer, large_font);
-  layer_add_child(weather_layer, text_layer_get_layer(wld->temp_layer));
+  temp_layer = text_layer_create(GRect(70, 19, 72, 80));
+  text_layer_set_background_color(temp_layer, GColorClear);
+  text_layer_set_text_alignment(temp_layer, GTextAlignmentCenter);
+  text_layer_set_font(temp_layer, large_font);
+  layer_add_child(weather_layer, text_layer_get_layer(temp_layer));
 
   // Add bitmap layer
-  wld->icon_layer = bitmap_layer_create(GRect(9, 13, 60, 60));
-  layer_add_child(weather_layer, bitmap_layer_get_layer(wld->icon_layer));
+  icon_layer = bitmap_layer_create(GRect(9, 13, 60, 60));
+  layer_add_child(weather_layer, bitmap_layer_get_layer(icon_layer));
 
-  wld->icon = NULL;
+  bitmap = NULL;
 
   return weather_layer;
 }
 
 static void weather_layer_set_temp(int temp){
-  WeatherLayerData *wld = layer_get_data(weather_layer);
-  snprintf(wld->temp_str, sizeof(wld->temp_str), "%i°", temp);
+  static char temp_str[6];
+  snprintf(temp_str, sizeof(temp_str), "%i°", temp);
 
-  text_layer_set_text(wld->temp_layer, wld->temp_str);
+  text_layer_set_text(temp_layer, temp_str);
 }
 
 static void weather_layer_set_icon(uint32_t icon){
-  WeatherLayerData *wld = layer_get_data(weather_layer);
-
-  GBitmap *new_icon = gbitmap_create_with_resource(icon);
+  GBitmap *new_bitmap = gbitmap_create_with_resource(icon);
   // Display the new bitmap
-  bitmap_layer_set_bitmap(wld->icon_layer, new_icon);
+  bitmap_layer_set_bitmap(icon_layer, new_bitmap);
 
-  // Destroy the ex-current icon if it existed
-  if (wld->icon != NULL) {
-    gbitmap_destroy(wld->icon);
+  // Destroy the ex-current bitmap if it existed
+  if (bitmap != NULL) {
+    gbitmap_destroy(bitmap);
   }
-  wld->icon = new_icon;
+  bitmap = new_bitmap;
 }
 
 void weather_layer_draw(){
@@ -77,16 +71,13 @@ void weather_layer_draw(){
 }
 
 void weather_layer_destroy(WeatherLayer* weather_layer){
-
-  WeatherLayerData *wld = layer_get_data(weather_layer);
-
-  text_layer_destroy(wld->temp_layer);
-  text_layer_destroy(wld->background_layer);
-  bitmap_layer_destroy(wld->icon_layer);
+  text_layer_destroy(temp_layer);
+  text_layer_destroy(background_layer);
+  bitmap_layer_destroy(icon_layer);
 
   // Destroy the previous bitmap if we have one
-  if (wld->icon != NULL){
-    gbitmap_destroy(wld->icon);
+  if (bitmap != NULL){
+    gbitmap_destroy(bitmap);
   }
   layer_destroy(weather_layer);
 
--- src/weather_layer.h
+++ src/weather_layer.h
@@ -2,5 +2,4 @@ typedef Layer WeatherLayer;
 
 WeatherLayer *weather_layer_create(GRect frame);
 void weather_layer_destroy();
-
 void weather_layer_draw();

5f651f3dddd8abb6ae0ab335f8831e7036566db2
John Hawthorn <john.hawthorn@gmail.com>
2016-09-22 22:39:14 -0700

    Start switching to npm+webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+module.exports = function(options) {
+  var path = require('path');
+  var webpack = require('webpack');
+  var config = {};
+
+  config.context = path.join(__dirname, 'app');
+
+  config.entry = {
+    default: './js/default.js'
+  };
+
+  config.output = {
+    path: path.join(__dirname, "src/js/"),
+    filename: "pebble-js-app.js"
+  }
+
+  config.plugins = [
+    new webpack.ProvidePlugin({
+      'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+    })
+  ];
+
+  config.module = {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
+
+  config.resolve = {
+    modules: [path.resolve(__dirname, "app"), "node_modules"]
+  };
+
+  return config;
+};
